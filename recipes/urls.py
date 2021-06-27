22c863586dccb9611b959a84f1b0ef91484a6dcb
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2021-06-27 16:04:59 -0400

    set up the url  route for recip '

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('', views.index, name='homepage')
+    path('', views.index, name='homepage'),
+    path('recipe_add/', views.recipe, name='recipe_add'),
+    path('recipe/', views.recipe_page, name='recipe')
 ]
