797e2607f3dc581e760f272a019391df713c6f2a
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2021-06-27 15:48:17 -0400

    updated the views on recipes to render to webpage.

--- recipes/views.py
+++ recipes/views.py
@@ -6,5 +6,33 @@ from recipes_user.models import RecipeUser
 
 def index(request):
     html = "index.html"
-    return render(request, html)
+    data = Food.objects.all()
+    context = {"data": data}
+    return render(request, html, context)
+
+def recipe(request):
+    html = "recipe.html"
+    if request.method == 'POST':
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Food.object.create(
+                food_title=data['food_title'],
+                ingredients=data['ingredients'],
+                directions=data['directions'],
+                food_author=data['food_image']
+            )
+            return HttpResponseRedirect(reverse('homepage'))
+    else:
+        form = RecipeForm()
+    context = {'form': form}
+    return render(request, html, context)
+
+def recipedetail(request, username):
+    html = "recipe_page.html"
+    author = RecipeUser.objects.get(username=username)
+    author = request.user
+    recipes = Food.objects.filter(food_author=author)
+    context = {"author":author, "recipes":recipes}
+    return render(request, html, context)
 
